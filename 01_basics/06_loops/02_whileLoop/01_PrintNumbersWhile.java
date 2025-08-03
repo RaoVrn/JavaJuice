e60ceda28a16d9a537025129c2b3d1b832af0f43
RaoVrn <prakash.varun.0305@gmail.com>
2025-08-04 00:11:24 +0530

    🔁 Print numbers from 0 to 10 using while loop

---
+++ 01_basics/06_loops/02_whileLoop/01_PrintNumbersWhile.java
@@ -0,0 +1,25 @@
+// 03_PrintNumbersWhile.java
+// JavaJuice - Homework: Print numbers from 0 to 10 using a while loop
+
+/*
+ * 📝 Homework:
+ * Write a Java program that:
+ * - Uses a `while` loop to print numbers from 0 to 10.
+ *
+ * ✅ Use only one println() inside the loop.
+ * ✅ Initialize and update the loop variable properly.
+ */
+
+import java.util.*;
+
+public class Print {
+    public static void main(String[] args) {
+       int i = 0; // Initialization
+
+        // Run the loop as long as i is less than or equal to 10
+        while (i <= 10) {
+            System.out.println(i); // Print current value of i
+            i++; // Increment i by 1
+        }
+    }
+}
