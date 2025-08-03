7c3e844b4a4b11ab22801fce0f5742c31cdb33aa
RaoVrn <prakash.varun.0305@gmail.com>
2025-08-04 00:03:06 +0530

    🔁 Print Hello World 3 times using loop

---
+++ 01_basics/06_loops/01_HelloLoop.java
@@ -0,0 +1,22 @@
+// 01_HelloLoop.java
+// JavaJuice - Homework: Print "Hello World!" 3 times using a for loop
+
+/*
+ * 📝 Homework:
+ * Write a Java program using a `for` loop to:
+ * - Print "Hello World!" three times.
+ *
+ * ✅ Use a `for` loop and avoid repeating println manually.
+ */
+
+import java.util.*;
+
+public class Print {
+    public static void main(String[] args) {
+
+        // Loop runs 3 times: i = 0, 1, 2
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Hello World!");
+        }
+    }
+}
